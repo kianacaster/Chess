fdf46b3e3765e15b004b9f0e3b8eb964593a81ef
Kian Acaster <acasterkian@gmail.com>
2021-10-16 00:27:46 +0100

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-for (let i = 1; i <= 7; i++) { 
-	const { tileX, tileY } = this.tile; 
-	let newX = tileX + i; 
-	let newY = tileY + i; 
-	if (newX < 8 && newY + i < 8) { 
-		this.legalMoves.push(board.tiles[newX][newY]) 
-	} 
-}
